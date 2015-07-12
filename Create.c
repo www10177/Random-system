acd999d4640188f89040dde1fa71b9733a053c2e
Rist <www10177@gmail.com>
2015-07-12 23:19:57 +0800

    Add name list(.name.lst)

--- Create.c
+++ Create.c
@@ -8,12 +8,13 @@ int main(void)
 	/*declare and initialize*/
 	char name[ 101 ], data[25];
 	FILE* stream;
-
+	FILE* list; 
 	/*To enter name*/
-	printf("Please Enter List Name\n");
+
+	printf("\nPlease Enter List Name\n");
 	printf("Limit: less than 100 characters\n");
 	gets(name);
-	printf("You Entered %s \n",name);
+	printf("You Entered %s \n\n",name);
 	/*With some bugs that cause segmentaion fualt*/
 	/*
 	 *int enter_name_end = 0;
@@ -38,9 +39,14 @@ int main(void)
 	 *    }
 	 *}
 	 */
+	/*Take the list name in the "list" list*/
+	if ( ( list=fopen(".name.lst","a") )== NULL ){
+		printf("ERROR: Open name list ERROR\n");
+	}
 
 	/*convert the name into the real datname*/
-	char name_temp[110];
+	char name_temp[110],name_orig[110];
+	strcpy(name_orig,name);
 	strcpy(name_temp,".");
 	strcat(name_temp,name);
 	strcat(name_temp,".dat");
@@ -48,8 +54,8 @@ int main(void)
 
 	/*Check if the List is already existed and create the stream(file)*/
 	if ( access(name, F_OK) == 0 ) {
-		printf("The file is already existed. Append the data to the List. \n");
-		if( (stream = fopen(name,"a") ) ==NULL ){
+		printf("The list %s is already existed. Append the data to the List. \n",name_orig);
+		if( (stream = fopen(name,"ab") ) ==NULL ){
 			printf("ERROR: Append error\n");
 			return 1;
 		}
@@ -59,8 +65,9 @@ int main(void)
 			printf("ERROR: create error\n");
 			return 1;
 		}
-	}
-
+		printf("The list %s is created\n",name_orig);
+		fprintf(list,"%s\n",name_orig);
+	} 
 	/*input data*/
 	printf("*****************\n");
 	printf("Please Enter Data\n");
@@ -68,9 +75,11 @@ int main(void)
 	printf("*****************\n");
 	while ( gets( data ) != NULL){
 		fprintf( stream, "%s\n", data);
-		printf("\tYou enter\t%s\n",data);
+		printf("\nYou enter\t%s\n\n",data);
 	}
-	printf("Input End\n");
+	printf("Input End\n\n");
+	fclose(stream);
+	fclose(list);
 	return 0;
 }
 
